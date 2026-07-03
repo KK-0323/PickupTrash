a7d6da6db545400db55941d0a43da53808f991f2
KK-0323 <240181@jc-21.jp>
2026-07-03 16:44:30 +0900

    生成処理を仮追加
    生成する処理をいったん追加

--- Assets/Scripts/RoadScroller.cs
+++ Assets/Scripts/RoadScroller.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RoadScroller : MonoBehaviour
@@ -8,6 +9,15 @@ public class RoadScroller : MonoBehaviour
     [Header("削除するZ座標")]
     [SerializeField] private float destroyZ = -20f;
 
+    [Header("生成するプレハブ")]
+    [SerializeField] private GameObject roadPrefab;
+
+    [Header("道路1枚のzの長さ")]
+    [SerializeField] private float roadLength = 30f;
+
+    // 道路を生成したかのフラグ
+    private bool roadSpawned = false;
+
     void Start()
     {
         Application.targetFrameRate = 60;
@@ -15,11 +25,26 @@ public class RoadScroller : MonoBehaviour
 
     void Update()
     {
+        // 手前移動
         transform.Translate(Vector3.back * scrSpeed * Time.deltaTime);
+
+        if (!roadSpawned && transform.position.z <= destroyZ + roadLength)
+        {
+            SpawnNextRoad();
+        }
 
         if(transform.position.z < destroyZ)
         {
             Destroy(gameObject);
         }
+    }
+
+    void SpawnNextRoad()
+    {
+        roadSpawned = true;
+        Debug.Log("生成したよ");
+
+        // 今の道路から道路の長さ分奥の座標を計算
+        Vector3 spawnPos = new Vector3(transform.position.x, transform.position.y, transform.position.z + (roadLength * 2));
     }
 }
