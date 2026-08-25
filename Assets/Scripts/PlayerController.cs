9fad192dca4e39552724f4ff103f7316f703e83a
KK-0323 <240181@jc-21.jp>
2026-08-25 14:37:06 +0900

    素材のフォルダ名変更とモーション再生
    各素材フォルダの名前を分かりやすく変更
    プレイヤーに静止時と移動時のモーションを追加し再生できた

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,24 +2,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float leftX = -2.0f;
-    public float rightX = 2.0f;
+    private Animator animator;
+    //public float leftX = -2.0f;
+    //public float rightX = 2.0f;
 
     void Start()
     {
         Application.targetFrameRate = 60;
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         // キーボード操作(デバッグ用)
-        if(Input.GetKeyDown(KeyCode.A))
-        {
-            this.transform.Translate(leftX, 0.0f, 0.0f);
-        }
-        if(Input.GetKeyDown(KeyCode.D))
-        {
-            this.transform.Translate(rightX, 0.0f, 0.0f);
-        }
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        bool isMoving = (horizontal != 0 || vertical != 0);
+
+        animator.SetBool("IsWalking", isMoving);
+        //if(Input.GetKeyDown(KeyCode.A))
+        //{
+        //    this.transform.Translate(leftX, 0.0f, 0.0f);
+        //}
+        //if(Input.GetKeyDown(KeyCode.D))
+        //{
+        //    this.transform.Translate(rightX, 0.0f, 0.0f);
+        //}
     }
 }
